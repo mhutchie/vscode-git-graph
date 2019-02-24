f4006976e028a9611605e4eacd9430f14d5bc4f5
Michael Hutchison <mhutchie@16right.com>
2019-02-24 23:44:41 +1100

    Set up CI with Azure Pipelines

--- .vscode/clean.js
+++ .vscode/clean.js
@@ -3,7 +3,7 @@ const path = require('path');
 const directory = './out';
 
 fs.readdir(directory, (err, files) => {
-	if (err) throw err;
+	if (err) return;
 
 	for (const file of files) {
 		fs.unlink(path.join(directory, file), err => {

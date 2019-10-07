465c57df200da354a3724ee822d5f4922bfe328c
Michael Hutchison <mhutchie@16right.com>
2019-10-08 10:42:34 +1100

    #192 Fixed handling of filenames containing # characters in the Diff view.

--- src/diffDocProvider.ts
+++ src/diffDocProvider.ts
@@ -92,7 +92,11 @@ export function encodeDiffDocUri(repo: string, filePath: string, commit: string,
 		let extIndex = data.filePath.indexOf('.', data.filePath.lastIndexOf('/') + 1);
 		extension = extIndex > -1 ? data.filePath.substring(extIndex) : '';
 	}
-	return vscode.Uri.parse(DiffDocProvider.scheme + ':file' + extension + '?' + Buffer.from(JSON.stringify(data)).toString('base64'));
+
+	return vscode.Uri.file('file' + extension).with({
+		scheme: DiffDocProvider.scheme,
+		query: Buffer.from(JSON.stringify(data)).toString('base64')
+	});
 }
 
 export function decodeDiffDocUri(uri: vscode.Uri): DiffDocUriData {

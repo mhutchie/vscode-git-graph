1492f57f8f667f8fcd41f08e52d8e3a0704d1b3f
Michael Hutchison <mhutchie@16right.com>
2019-11-12 22:41:10 +1100

    #209 Use the Node.js File System Module instead of the (default) Electron version of the same module (which handles .asar files differently).

--- .vscode/package-src.js
+++ .vscode/package-src.js
@@ -5,6 +5,12 @@ const SRC_DIRECTORY = './src/';
 const OUT_DIRECTORY = './out/';
 const ASKPASS_DIRECTORY = '/askpass';
 
+fs.readdirSync(OUT_DIRECTORY).forEach(filename => {
+	if (filename.endsWith('.js')) {
+		fs.writeFileSync(OUT_DIRECTORY + filename, fs.readFileSync(OUT_DIRECTORY + filename).toString().replace(/require\("fs\"\)/g, 'require("original-fs")'));
+	}
+});
+
 fs.readdirSync(SRC_DIRECTORY + ASKPASS_DIRECTORY).forEach(filename => {
 	if (!filename.endsWith('.ts')) {
 		fs.writeFileSync(OUT_DIRECTORY + ASKPASS_DIRECTORY + '/' + filename, fs.readFileSync(SRC_DIRECTORY + ASKPASS_DIRECTORY + '/' + filename).toString());

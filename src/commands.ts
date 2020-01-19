771081afa052f91c7d50d28378e4ce2120a04288
Michael Hutchison <mhutchie@16right.com>
2020-01-19 17:25:31 +1100

    Refactor all extension commands to use the new CommandManager.

--- src/commands.ts
+++ src/commands.ts
@@ -1,48 +1,127 @@
 import * as vscode from 'vscode';
+import { AvatarManager } from './avatarManager';
+import { getConfig } from './config';
+import { DataSource } from './dataSource';
+import { ExtensionState } from './extensionState';
+import { GitGraphView } from './gitGraphView';
+import { Logger } from './logger';
 import { RepoManager } from './repoManager';
-import { getPathFromUri, getRepoName, GitExecutable, isPathInWorkspace, showErrorMessage, showInformationMessage, UNABLE_TO_FIND_GIT_MSG } from './utils';
+import { getPathFromUri, getRepoName, GitExecutable, isPathInWorkspace, resolveToSymbolicPath, showErrorMessage, showInformationMessage, UNABLE_TO_FIND_GIT_MSG } from './utils';
 
 
-export function addGitRepository(gitExecutable: GitExecutable | null, repoManager: RepoManager) {
-	if (gitExecutable === null) {
-		showErrorMessage(UNABLE_TO_FIND_GIT_MSG);
-		return;
+export class CommandManager implements vscode.Disposable {
+	private readonly extensionPath: string;
+	private readonly avatarManager: AvatarManager;
+	private readonly dataSource: DataSource;
+	private readonly extensionState: ExtensionState;
+	private readonly logger: Logger;
+	private readonly repoManager: RepoManager;
+	private gitExecutable: GitExecutable | null;
+	private subscriptions: vscode.Disposable[] = [];
+
+	constructor(extensionPath: string, avatarManger: AvatarManager, dataSource: DataSource, extensionState: ExtensionState, logger: Logger, repoManager: RepoManager, gitExecutable: GitExecutable | null) {
+		this.extensionPath = extensionPath;
+		this.avatarManager = avatarManger;
+		this.dataSource = dataSource;
+		this.extensionState = extensionState;
+		this.logger = logger;
+		this.repoManager = repoManager;
+		this.gitExecutable = gitExecutable;
+
+		this.registerCommand('git-graph.view', (arg) => this.view(arg));
+		this.registerCommand('git-graph.addGitRepository', () => this.addGitRepository());
+		this.registerCommand('git-graph.removeGitRepository', () => this.removeGitRepository());
+		this.registerCommand('git-graph.clearAvatarCache', () => this.clearAvatarCache());
+		this.registerCommand('git-graph.endAllWorkspaceCodeReviews', () => this.endAllWorkspaceCodeReviews());
 	}
 
-	vscode.window.showOpenDialog({ canSelectFiles: false, canSelectFolders: true, canSelectMany: false }).then(uris => {
-		if (uris && uris.length > 0) {
-			let path = getPathFromUri(uris[0]);
-			if (isPathInWorkspace(path)) {
-				repoManager.registerRepo(path, false).then(status => {
-					if (status.error === null) {
-						showInformationMessage('The repository "' + status.root! + '" was added to Git Graph.');
-					} else {
-						showErrorMessage(status.error + ' Therefore it could not be added to Git Graph.');
-					}
-				});
-			} else {
-				showErrorMessage('The folder "' + path + '" is not within the opened Visual Studio Code workspace, and therefore could not be added to Git Graph.');
-			}
-		}
-	}, () => { });
-}
-
-export function removeGitRepositoy(gitExecutable: GitExecutable | null, repoManager: RepoManager) {
-	if (gitExecutable === null) {
-		showErrorMessage(UNABLE_TO_FIND_GIT_MSG);
-		return;
+	public dispose() {
+		this.subscriptions.forEach((subscription) => {
+			subscription.dispose();
+		});
+		this.subscriptions = [];
 	}
 
-	let repoPaths = Object.keys(repoManager.getRepos());
-	let items: vscode.QuickPickItem[] = repoPaths.map(path => ({ label: getRepoName(path), description: path }));
+	public setGitExecutable(gitExecutable: GitExecutable | null) {
+		this.gitExecutable = gitExecutable;
+	}
 
-	vscode.window.showQuickPick(items, { canPickMany: false, placeHolder: 'Select a repository to remove from Git Graph' }).then((item) => {
-		if (item && item.description !== undefined) {
-			if (repoManager.ignoreRepo(item.description)) {
-				showInformationMessage('The repository "' + item.label + '" was removed from Git Graph.');
-			} else {
-				showErrorMessage('The repository "' + item.label + '" is not known to Git Graph.');
+	private registerCommand(command: string, callback: (...args: any[]) => any) {
+		this.subscriptions.push(vscode.commands.registerCommand(command, callback));
+	}
+
+
+	/* Commands */
+
+	private async view(arg: any) {
+		let loadRepo: string | null = null;
+
+		if (typeof arg === 'object' && arg.rootUri) {
+			// If command is run from the Visual Studio Code Source Control View, load the specific repo
+			const repoPath = getPathFromUri(arg.rootUri);
+			loadRepo = await this.repoManager.getKnownRepo(repoPath);
+			if (loadRepo === null) {
+				// The repo is not currently known, add it
+				loadRepo = (await this.repoManager.registerRepo(await resolveToSymbolicPath(repoPath), true)).root;
 			}
+		} else if (getConfig().openToTheRepoOfTheActiveTextEditorDocument && vscode.window.activeTextEditor) {
+			// If the config setting is enabled, load the repo containing the active text editor document
+			loadRepo = this.repoManager.getRepoContainingFile(getPathFromUri(vscode.window.activeTextEditor.document.uri));
 		}
-	}, () => { });
+
+		GitGraphView.createOrShow(this.extensionPath, this.dataSource, this.extensionState, this.avatarManager, this.repoManager, this.logger, loadRepo);
+	}
+
+	private addGitRepository() {
+		if (this.gitExecutable === null) {
+			showErrorMessage(UNABLE_TO_FIND_GIT_MSG);
+			return;
+		}
+
+		vscode.window.showOpenDialog({ canSelectFiles: false, canSelectFolders: true, canSelectMany: false }).then(uris => {
+			if (uris && uris.length > 0) {
+				let path = getPathFromUri(uris[0]);
+				if (isPathInWorkspace(path)) {
+					this.repoManager.registerRepo(path, false).then(status => {
+						if (status.error === null) {
+							showInformationMessage('The repository "' + status.root! + '" was added to Git Graph.');
+						} else {
+							showErrorMessage(status.error + ' Therefore it could not be added to Git Graph.');
+						}
+					});
+				} else {
+					showErrorMessage('The folder "' + path + '" is not within the opened Visual Studio Code workspace, and therefore could not be added to Git Graph.');
+				}
+			}
+		}, () => { });
+	}
+
+	private removeGitRepository() {
+		if (this.gitExecutable === null) {
+			showErrorMessage(UNABLE_TO_FIND_GIT_MSG);
+			return;
+		}
+
+		let repoPaths = Object.keys(this.repoManager.getRepos());
+		let items: vscode.QuickPickItem[] = repoPaths.map(path => ({ label: getRepoName(path), description: path }));
+
+		vscode.window.showQuickPick(items, { canPickMany: false, placeHolder: 'Select a repository to remove from Git Graph' }).then((item) => {
+			if (item && item.description !== undefined) {
+				if (this.repoManager.ignoreRepo(item.description)) {
+					showInformationMessage('The repository "' + item.label + '" was removed from Git Graph.');
+				} else {
+					showErrorMessage('The repository "' + item.label + '" is not known to Git Graph.');
+				}
+			}
+		}, () => { });
+	}
+
+	private clearAvatarCache() {
+		this.avatarManager.clearCache();
+	}
+
+	private endAllWorkspaceCodeReviews() {
+		this.extensionState.endAllWorkspaceCodeReviews();
+		showInformationMessage('Ended All Code Reviews in Workspace');
+	}
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,14 @@
 import * as vscode from 'vscode';
 import { AvatarManager } from './avatarManager';
-import { addGitRepository, removeGitRepositoy } from './commands';
+import { CommandManager } from './commands';
 import { getConfig } from './config';
 import { DataSource } from './dataSource';
 import { DiffDocProvider } from './diffDocProvider';
 import { ExtensionState } from './extensionState';
-import { GitGraphView } from './gitGraphView';
 import { Logger } from './logger';
 import { RepoManager } from './repoManager';
 import { StatusBarItem } from './statusBarItem';
-import { findGit, getGitExecutable, getPathFromUri, GitExecutable, resolveToSymbolicPath, showErrorMessage, showInformationMessage, UNABLE_TO_FIND_GIT_MSG } from './utils';
+import { findGit, getGitExecutable, GitExecutable, showErrorMessage, showInformationMessage, UNABLE_TO_FIND_GIT_MSG } from './utils';
 
 
 export async function activate(context: vscode.ExtensionContext) {
@@ -33,39 +32,10 @@ export async function activate(context: vscode.ExtensionContext) {
 	const avatarManager = new AvatarManager(dataSource, extensionState, logger);
 	const statusBarItem = new StatusBarItem();
 	const repoManager = new RepoManager(dataSource, extensionState, statusBarItem, logger);
+	const commandManager = new CommandManager(context.extensionPath, avatarManager, dataSource, extensionState, logger, repoManager, gitExecutable);
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('git-graph.view', async (args) => {
-			let loadRepo: string | null = null;
-
-			if (typeof args === 'object' && args.rootUri) {
-				// If command is run from the Visual Studio Code Source Control View, load the specific repo
-				const repoPath = getPathFromUri(args.rootUri);
-				loadRepo = await repoManager.getKnownRepo(repoPath);
-				if (loadRepo === null) {
-					// The repo is not currently known, add it
-					loadRepo = (await repoManager.registerRepo(await resolveToSymbolicPath(repoPath), true)).root;
-				}
-			} else if (getConfig().openToTheRepoOfTheActiveTextEditorDocument && vscode.window.activeTextEditor) {
-				// If the config setting is enabled, load the repo containing the active text editor document
-				loadRepo = repoManager.getRepoContainingFile(getPathFromUri(vscode.window.activeTextEditor.document.uri));
-			}
-
-			GitGraphView.createOrShow(context.extensionPath, dataSource, extensionState, avatarManager, repoManager, logger, loadRepo);
-		}),
-		vscode.commands.registerCommand('git-graph.addGitRepository', () => {
-			addGitRepository(gitExecutable, repoManager);
-		}),
-		vscode.commands.registerCommand('git-graph.removeGitRepository', () => {
-			removeGitRepositoy(gitExecutable, repoManager);
-		}),
-		vscode.commands.registerCommand('git-graph.clearAvatarCache', () => {
-			avatarManager.clearCache();
-		}),
-		vscode.commands.registerCommand('git-graph.endAllWorkspaceCodeReviews', () => {
-			extensionState.endAllWorkspaceCodeReviews();
-			showInformationMessage('Ended All Code Reviews in Workspace');
-		}),
+		commandManager,
 		vscode.workspace.registerTextDocumentContentProvider(DiffDocProvider.scheme, new DiffDocProvider(dataSource)),
 		vscode.workspace.onDidChangeConfiguration(e => {
 			if (e.affectsConfiguration('git-graph.showStatusBarItem')) {
@@ -82,6 +52,7 @@ export async function activate(context: vscode.ExtensionContext) {
 					gitExecutable = exec;
 					extensionState.setLastKnownGitPath(gitExecutable.path);
 					dataSource.setGitExecutable(gitExecutable);
+					commandManager.setGitExecutable(gitExecutable);
 
 					let msg = 'Git Graph is now using ' + gitExecutable.path + ' (version: ' + gitExecutable.version + ')';
 					showInformationMessage(msg);
